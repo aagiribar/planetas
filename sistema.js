c94f059886d31a3a77b88b42bda9cc8f9e3268e7
Glitch (glitch-hello-website) <none>
2024-10-24 15:34:39 +0000

    💷🎸 Checkpoint ./sistema.js:70372692/689 ./styles.css:70372692/139 ./server.js:70372692/3 ./package.json:70372692/1171

--- sistema.js
+++ sistema.js
@@ -3,10 +3,25 @@ import * as THREE from 'three';
 let escena, renderer, camara;
 
 init();
+animationLoop();
 
 function init() {
   escena = new THREE.Scene();
+  camara = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camara.position.set(0, 0, 10);
   
-  camara = new THREE.PerspectiveCamera();
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
   
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material );
+  escena.add( cube );
+  
+}
+
+function animationLoop() {
+  requestAnimationFrame(animationLoop);
+  renderer.render(escena, camara);
 }
