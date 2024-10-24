d050441e33c1e7350d7be81b3889465ae2dd1a3a
Glitch (glitch-hello-website) <none>
2024-10-24 15:57:16 +0000

    🙇🏃 Checkpoint ./.glitch-assets:70372692/421 ./public/sistema.js:70372692/1318

--- public/sistema.js
+++ public/sistema.js
@@ -18,11 +18,22 @@ function init() {
   
   let camcontrols = new OrbitControls(camara, renderer.domElement);
   
-  Estrella(radio, textura) {
-    
-  }
+  const tx_sol = new THREE.TextureLoader().load("https://cdn.glitch.global/4591fef6-cf3a-4142-af6c-7c82ef7b6add/sunmap.jpg?v=1729784770619");
+  Estrella(2, tx_sol)
   
 }
+
+function Estrella(radio, textura = undefined) {
+    let geometria = new THREE.SphereGeometry(radio, 10, 10);
+    let material = new THREE.MeshBasicMaterial({color: 0xffff00});
+  
+    if (textura != undefined) { 
+      material.map = textura;
+    }
+    
+    estrella = new THREE.Mesh(geometria, material);
+    escena.add(estrella);
+  }
 
 function animationLoop() {
   requestAnimationFrame(animationLoop);
