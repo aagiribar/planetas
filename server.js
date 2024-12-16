5e08a1b07be749656398756dbb8fdf3752c7ac48
Agustín Alfonso González Iríbar <agustin.gonzalez112@alu.ulpgc.es>
2024-12-16 18:15:49 +0000

    Añadido .gitignore, eliminado un archivo no importante y pequeños cambios en server.js

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get("/", function (request, response) {
 app.use(express.static("assets"));
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(/*process.env.PORT*/ 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
